defab5757fad3a1e5002f1a60adfd9c3990d1983
Kevin Altman <kevin@itsthatguy.com>
2015-07-09 13:26:21 -0400

    Ensures bower_components directory exists

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,3 +1,11 @@
 'use strict';
 
+// Make sure a bower components directory exists
+// Side-effect of not having any bower dependencies
+var fs = require('fs');
+var data = fs.readFileSync('./.bowerrc', 'utf-8');
+var bowerDirectory = JSON.parse(data).directory;
+if (!fs.existsSync(bowerDirectory)) { fs.mkdirSync(bowerDirectory); }
+
+// Setup gulp
 require('./gulp/index');
